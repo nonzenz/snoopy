c18b3867347e7d691f5c9ceaf0f93291fd2d76ae
Robert Väljur <robert.valjur@gmail.com>
2013-03-03 11:10:29 +0200

    JavaScript

--- Valimised/war/html/js/main.js
+++ Valimised/war/html/js/main.js
@@ -3,3 +3,18 @@ function isMobile(){
 		window.location = "MobHome.html";
 	}
 }
+
+var $j = jQuery.noConflict();
+
+$j(document).ready(function () {
+    
+	$j("a").click(function(){
+    	$j.ajax({
+    		url: $j(this).attr("href"),
+    		success: function(response) {
+    			$j("#content").html(response);
+    		}
+    	});
+    	});
+    	return false;
+});
