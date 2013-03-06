b0f2f9d80f279945bfe72518779a76e13f485a20
Robert Väljur <robert.valjur@gmail.com>
2013-03-06 23:37:41 +0200

    Javascript

--- Valimised/war/html/js/main.js
+++ Valimised/war/html/js/main.js
@@ -4,6 +4,19 @@ function isMobile(){
 	}
 }
 
+
+function myFunction(){
+	document.body.style.cursor  = 'wait';
+	setTimeout('stopCursor()',1000);
+}
+
+function stopCursor(){
+	document.body.style.cursor  = 'default';
+}
+
+var busy = true;
+
+
 var $j = jQuery.noConflict();
 
 $j(document).ready(function () {
