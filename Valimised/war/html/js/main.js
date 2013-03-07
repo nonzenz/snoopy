a686d709c4b6d620ce0445557cafe654812841bd
Robert Väljur <robert.valjur@gmail.com>
2013-03-07 20:12:31 +0200

    Proov

--- Valimised/war/html/js/main.js
+++ Valimised/war/html/js/main.js
@@ -4,7 +4,6 @@ function isMobile(){
 	}
 }
 
-
 function myFunction(){
 	document.body.style.cursor  = 'wait';
 	setTimeout('stopCursor()',1000);
@@ -13,9 +12,6 @@ function myFunction(){
 function stopCursor(){
 	document.body.style.cursor  = 'default';
 }
-
-var busy = true;
-
 
 var $j = jQuery.noConflict();
 
