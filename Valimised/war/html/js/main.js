c67155010ae902d1a7f3f33617f020e99db54c35
Helina <Helina@Helina-PC.lan>
2013-03-11 01:13:06 +0200

    Muutused.

--- Valimised/war/html/js/main.js
+++ Valimised/war/html/js/main.js
@@ -18,10 +18,10 @@ function validate(){
 	var y = document.getElementById("era").value;
 	var x = document.getElementById("piir").value;
 	if (y == 0) {
-		document.getElementById("message").innerHTML="Erakond valimata!"; status = false;
+		document.getElementById("message").innerHTML="     Erakond valimata!     "; status = false;
 	}
 	if (x == 0) {
-		document.getElementById("message2").innerHTML="Piirkond valimata!"; status = false;
+		document.getElementById("message2").innerHTML="     Piirkond valimata!     "; status = false;
 	} 
 	return status;
 };
